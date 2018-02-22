3ac19e3c25dfdf5a11daf95292d7b3d9eb4b85c4
paalaro <paal-anders@hotmail.com>
2018-02-22 21:04:41 +0100

    Registrering, reseting av passord, delvis login

---
+++ src/mail.js
@@ -0,0 +1,33 @@
+class MailService {
+  sendMail(recieverAdress, mailSubject, mailText) {
+    let nodemailer = require('nodemailer');
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'gruppe8passord@gmail.com',
+            pass: '0O1nNGBm'
+        }
+    });
+
+    let mailOptions = {
+        from: '"Gruppe 8" <gruppe8passord@gmail.com>',
+        to: recieverAdress,
+        subject: mailSubject,
+        text: mailText
+    };
+
+      transporter.sendMail(mailOptions, function(error,info) {
+        if (error) {
+          return console.log(error);
+        }
+        else {
+          console.log('Message sent: %s', info.messageId);
+        }
+      })
+    };
+}
+
+let mailService = new MailService();
+
+export { mailService };
